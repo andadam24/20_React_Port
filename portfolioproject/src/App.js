efaafab10ce921b9850046970009db180f98fbff
Adam And <andadam24@gmail.com>
2021-08-30 18:07:12 -0400

    initial commits

---
+++ portfolioproject/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App(){
+
+  return(
+    <div className="app">
+      <header>
+        <h1 className="title">Adam Andrzejczak's Portfolio</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
