7476083df599147ea2ded555fe0847e5338097b4
Adam And <andadam24@gmail.com>
2021-09-04 21:51:53 -0400

    updated header with my title

--- portfolioproject/src/components/header.js
+++ portfolioproject/src/components/header.js
@@ -4,35 +4,7 @@ import React from 'react';
 function Header(){
 
   return(
-    <header>
-
-        <div className="title fs-1"> 
-
-            Adam Andrzejczak's Portfolio
-
-        </div>
-
-        <nav className="navbar">
-             <ul className="navBarList">
-                <li>
-                    <a href="#">About Me</a>
-                </li>
-            
-             
-                <li>
-                    <a href="#">Portfolio</a>
-                </li>
-            
-                <li>
-                    <a href="https://github.com/andadam24">Resume</a>
-                </li>
-                <li>
-                    <a href="https://github.com/andadam24">Contact</a>
-                </li>
-            </ul>
-        </nav>
-
-    </header>
+    <h1 className="title"> Adam Andrzejczak's Portfolio</h1>
   );
 }
 
