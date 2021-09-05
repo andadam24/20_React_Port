636b5d18c7d7c0dd584fcfb28e2c486d7fa2550f
Adam And <andadam24@gmail.com>
2021-09-04 22:00:13 -0400

    updated contact page

--- portfolioproject/src/pages/contact.js
+++ portfolioproject/src/pages/contact.js
@@ -3,15 +3,37 @@ import React from 'react';
 function Contact(){
 
     return(
-      <div className="container-fluid">
-        
-        <card>
-          
-        
+      <div>
+        <form>
+          <h3>Contact</h3>
 
-        </card>
-                       
-  
+          <label> Name 
+            <br />
+            <input
+              type="text"
+              placeholder="Name"
+            />
+          </label>
+          <br />
+          <label> Email Address
+            <br />
+            <input
+              type="email"
+              placeholder="Email Address"
+            />
+          </label>
+          <br />
+          <label> Message
+            <br />
+            <input
+              type="text"
+              placeholder="Type message here ..."
+              className="message"
+            />
+          </label>
+          <br />
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
