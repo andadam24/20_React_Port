7b11013f373606f7d239ba8a24eed50170c99a7e
Adam And <andadam24@gmail.com>
2021-09-04 21:50:04 -0400

    updated app.js folder

--- portfolioproject/src/App.js
+++ portfolioproject/src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import './App.css';
+import './index.css';
+import { BrowserRouter, Switch, Route, Link, } from 'react-router-dom';
 import Header from './components/header';
-import Footer from './components/footer';
+import About from './pages.about';
+import Contact from './pages/contact';
 import Homepage from './pages/homepage';
+import Resume from '.pages/resume'
+import Footer from './components/footer';
 
 function App(){
 
   return(
-    <div className="app">
-
+   <BrowserRouter>
+    <div className="container">
       <Header />
-      
+      <div className="heading">
+        <Link to="/" className="links"> Home Page</Link>
+        <Link to="departments" className="links"> About</Link>
+        <Link to="/jobtitles" className="links"> Contact</Link>
+        <Link to="/employees" className="links"> Resume</Link>
+        </div>
+        <Switch>
+          <Route path='/' component={Homepage} exact />
+          <Route path='/departments' component={About} />
+          <Route path='/jobtitles' component={Contact} />
+          <Route path='/employees' component={Resume} />
 
-        <Homepage />
-
-
-
-
-
-      <Footer />
+          </Switch>
     </div>
+  </BrowserRouter>
   );
 }
 
