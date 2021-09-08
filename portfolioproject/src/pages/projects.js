59e772cbb94740b018644d3b6583e1d39721fc13
Adam And <andadam24@gmail.com>
2021-09-08 13:15:48 -0400

    finished resume page

--- portfolioproject/src/pages/projects.js
+++ portfolioproject/src/pages/projects.js
@@ -3,15 +3,36 @@ import React from 'react';
 function Homepage(){
 
     return(
-      <div className="container-fluid">
-        
-        <card>
-          
-
-
-
-        </card>
-                       
+<div className="container">  
+        <div className="d-flex justify-content-center">
+        <div className="col">
+            <div className="container text-white">
+                <h4 className="name text-white">CovTracker</h4>
+                    <img src="images/CovTracker.png" className="img.center" alt="CovTracker site" />
+                   <p>CovTacker was an app developed by classmates and me to help track the current number of confirmed covid casses and a person current location.</p>
+                   <p>Click the button below to check it out!</p>
+                   <a className="btn btn-primary" href="https://jamesljenks.github.io/07_Project-1_CovTrack/" role="button">CovTracker</a>
+            </div>
+        </div>
+        <div className="col">
+            <div class="container text-white">
+                <h4 className="name text-white">DayPlanner</h4>
+                    <img src="images/DayPlanner.png" className="img.center" alt="DayPlanner site" />
+                   <p>DayPlanner is an app that allows you to do exactly what the title alludes to: Plan your day. The interactive app will change colors depending on the time of the day and if the task is passed it's scheduled time.</p>
+                   <p>Click the button below to check it out!</p>
+                   <a className="btn btn-primary" href="https://andadam24.github.io/05_AAs_WorkDayScheduler/" role="button">DayPlanner</a>
+            </div>
+        </div>
+        <div className="col">
+            <div className="container text-white">
+                <h4 className="name text-white">RePost</h4>
+                    <img src="images/RePost.png" className="img.center" alt="RePost site" />
+                   <p>RePost is a take on social media, where it allows you to share your thoughts and 'RePost' other user's thoughts.</p>
+                   <p>Click the button below to check it out!</p>
+                   <a className="btn btn-primary" href="https://repostproject.herokuapp.com/" role="button">RePost</a>
+            </div>
+        </div>
+        </div>
   
       </div>
     );
--- portfolioproject/src/pages/resume.js
+++ portfolioproject/src/pages/resume.js
@@ -3,18 +3,18 @@ import React from 'react';
 function Resume(){
 
     return(
-      <div className="container-fluid">
-        
-        <card>
-          
+      
+        <div className="container">
+        <h3>Resume</h3>
+          <div className="row">
+            <div className="col-12">
+              <td onClick={()=> window.open("/images/Adam Andrzejczak Resume10241024_1.jpg","_blank")}>Click here to view Resume</td>
+            </div>
+            
 
-
-
-        </card>
-                       
-  
+        </div>
       </div>
     );
   }
   
-  export default Resume;
+  export default Resume
